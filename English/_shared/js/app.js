2e5128e23e37b25059ed04548f0dd91610dffa60
Farid <farid.hammouten@gmail.con>
2019-11-08 14:47:21 +0900

    add img btn

---
+++ English/_shared/js/app.js
@@ -0,0 +1,15 @@
+window.onload = hideImages;
+
+function showImages(){
+    let images = document.getElementsByTagName("img");
+    for(let i = 0; i<images.length; i++){
+        images[i].classList.remove("d-none");
+    }
+}
+
+function hideImages(){
+    let images = document.getElementsByTagName("img");
+    for(let i = 0; i<images.length; i++){
+        images[i].classList.add("d-none");
+    }
+}
